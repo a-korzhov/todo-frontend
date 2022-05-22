d7f3ea7a74b900dd0089cbb3a76737fbcc7c5325
Andrew <andrew.korzhov96@gmail.com>
2022-05-22 20:00:47 +0300

    make avatar cirle

--- src/components/image/SettingsAvatar.jsx
+++ src/components/image/SettingsAvatar.jsx
@@ -4,7 +4,12 @@ import {UserStatusIcon} from "../img";
 const SettingsAvatar = ({avatar, status}) => {
     return (
         <div className="col-md-4 p-2" style={{display: "inline-flex"}}>
-            <img src={avatar} className="img-fluid rounded-circle" alt="Avatar"/>
+            <img
+                src={avatar}
+                className="img-fluid rounded-circle"
+                alt="Avatar"
+                style={{clipPath: "circle()"}}
+            />
             <UserStatusIcon status={status}/>
         </div>
     );
